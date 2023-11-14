108b2c6fcba863a2c5657a610ca8590f5b5cff23
DGWHoang <hoang-dm@dgw.asia>
2023-11-14 13:35:45 +0700

    settup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import Nav from './views/Nav';
+
+
+
+const App = () => {
+
+  const handleClick = () => {
+    console.log('handleClick');
+  }
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={() => handleClick()}>Click me</button>
       </header>
     </div>
   );
---
+++ src/views/Nav.js
@@ -0,0 +1,14 @@
+import '../views/Nav.scss';
+
+const Nav = () => {
+    return (
+        <ul>
+            <li><a className="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    )
+}
+
+export default Nav
