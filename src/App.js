82854f084ce5f5b0dce8f5e619f33bdf76982cd8
DGWHoang <hoang-dm@dgw.asia>
2023-11-14 14:40:46 +0700

    useState hook

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.scss';
 
 import Nav from './views/Nav';
 
-
+import { useState } from 'react';
 
 const App = () => {
+  let [state, setState] = useState({
+    name: 'Hoang Minh',
+    age: 28
+  })//Destructuring Assignment
 
   const handleClick = () => {
-    console.log('handleClick');
+    console.log('handleClick', state);
+    setState(state.name);
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value
+    });
   }
 
   return (
@@ -16,6 +29,11 @@ const App = () => {
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>My name is {state.name}</p>
+        <input
+          type="text" value={state.name} name='name'
+          onChange={(e) => handleInputChange(e)}
+        />
         <button type="button" onClick={() => handleClick()}>Click me</button>
       </header>
     </div>
